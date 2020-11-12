9cabcaab7f16fb1534b0737de67dad467bb51934
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2020-11-11 20:12:15 -0500

    Create Visual Studio Solution

---
+++ BSU.StudentShowcase/Controllers/MultiProjectController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BSU.StudentShowcase.ViewModels;
+
+namespace BSU.StudentShowcase.Controllers
+{
+    public class MultiProjectController : Controller
+    {
+        private readonly ILogger<MultiProjectController> _logger;
+
+        public MultiProjectController(ILogger<MultiProjectController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BSU.StudentShowcase/ViewModels/MultiProject/ProjectPanelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSU.StudentShowcase.Frontend.ViewModels.MultiProject
+{
+    public class ProjectPanelViewModel
+    {
+        public string ProjectName { get; set; }
+        public string StudentName { get; set; }
+        public string Category { get; set; }
+        public string School { get; set; }
+        public string ImagePreviewUrl { get; set; }
+    }
+}
