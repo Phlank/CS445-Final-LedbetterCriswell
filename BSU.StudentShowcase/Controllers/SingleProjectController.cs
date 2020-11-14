59445f4b9545d8f966c7d9d24537a17069714436
Zachary Criswell <zachary.criswell@gmail.com>
2020-11-13 23:54:08 -0500

    gh-2: added a funcational single Project with a emmeded Youtube Video

--- BSU.StudentShowcase/Controllers/SingleProjectController.cs
+++ BSU.StudentShowcase/Controllers/SingleProjectController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BSU.StudentShowcase.Frontend.ViewModels.SingleProject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,32 @@ using System.Threading.Tasks;
 
 namespace BSU.StudentShowcase.Frontend.Controllers
 {
-    public class HomeController : Controller
+    public class SingleProjectController : Controller
     {
+        private readonly ILogger<SingleProjectController> _logger;
+
+        public SingleProjectController(ILogger<SingleProjectController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = new SingleProjectViewModel
+            {
+
+                Catagory = "America",
+                Description = "America is Great",
+                ProjectName = "Why America is Great",
+                ProjectType = "Video",
+                School = "Trump University",
+                StudentName = "Cheato King",
+                DateUploaded = "May 5",
+                VideoURL = "https://www.youtube.com/embed/8N5k3NeGoCw"
+
+
+            };
+            return View(model);
         }
     }
 }
--- BSU.StudentShowcase/ViewModels/SingleProject/SingleProjectViewModel.cs
+++ BSU.StudentShowcase/ViewModels/SingleProject/SingleProjectViewModel.cs
@@ -7,5 +7,17 @@ namespace BSU.StudentShowcase.Frontend.ViewModels.SingleProject
 {
     public class SingleProjectViewModel
     {
+        public string StudentName { get; set; }
+        public string School { get; set; }
+        public string ProjectName { get; set; }
+        public string Catagory { get; set; }
+        public string ProjectType { get; set; }
+        public string Description { get; set; }
+        public string DateUploaded { get; set; }
+        public string Summary { get; set; }
+        public string Text { get; set; }
+        public string BrochureURL { get; set; }
+        public string VideoURL { get; set; }
+
     }
 }
