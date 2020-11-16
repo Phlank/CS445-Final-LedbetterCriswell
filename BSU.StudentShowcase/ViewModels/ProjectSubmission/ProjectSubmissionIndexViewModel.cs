a916fddf56d2237465e5e8e56c8d28080aa20cd3
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2020-11-16 18:58:36 -0500

    gh-3: Completed validation for project submission
    Added a small bit of formatting to site.css, added options to the MVC builder in Startup.

---
+++ BSU.StudentShowcase/ViewModels/ProjectSubmission/ProjectSubmissionIndexViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSU.StudentShowcase.Frontend.ViewModels.ProjectSubmission
+{
+    public class ProjectSubmissionIndexViewModel
+    {
+        public int SubmissionId { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        public string StudentName { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        public string SchoolName { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        public string AdvisorName { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        public string ProjectType { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "This is a required field.")]
+        public string Content { get; set; }
+        public string AdditionalNotes { get; set; }
+        public DateTime SubmittedOn { get; set; }
+    }
+}
