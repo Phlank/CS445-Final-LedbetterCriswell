ff7e5dc75879fea6d9c91548e2636576e18e8c06
Zachary Criswell <zachary.criswell@gmail.com>
2020-11-15 20:51:03 -0500

    gh-4: Added Judge application

---
+++ BSU.StudentShowcase/Controllers/JudgeApplicationController.cs
@@ -0,0 +1,26 @@
+﻿using BSU.StudentShowcase.Frontend.ViewModels.JudgeApplication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSU.StudentShowcase.Frontend.Controllers
+{
+    public class JudgeApplicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new JudgeApplicationIndexViewModel
+            {
+
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Submit(JudgeApplicationIndexViewModel result)
+        {
+            return View();
+        }
+    }
+}
---
+++ BSU.StudentShowcase/ViewModels/JudgeApplication/JudgeApplicationIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSU.StudentShowcase.Frontend.ViewModels.JudgeApplication
+{
+    public class JudgeApplicationIndexViewModel
+    {
+        public int SubmissionId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public string Username { get; set; }
+    }
+}
